28bf17f41b29b2096143ff8af57a77494a554edd
caio lucena <caio.lcolaco@gmail.com>
2022-02-21 13:46:41 -0300

    knowing Promices

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,5 +1,29 @@
+const Category = require('../models/Category')
+const Product = require('../models/Product')
+
 module.exports = {
     create(req, res){
-        return res.render("products/create.njk")
+        Category.all().then(function(results) {
+
+            const categories = results.rows
+
+            return res.render("products/create.njk", { categories })
+        }).catch(function(err) {
+            throw new Error(err)
+        })
+
+    },
+    post(req, res) {
+        const keys = Object.keys(req.body)
+
+        for( key of keys) {
+            if (req.body[key] == "") {
+                return res.send('Please, fill all fields!')
+            }
+        }
+
+        Product.create(req.body).then(function() {
+            
+        })
     }
 }
